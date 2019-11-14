42ed8f44dd0560d4cb563d648c60822bac7e2688
willdunklin <54944980+willdunklin@users.noreply.github.com>
2019-11-14 09:34:13 -0500

    Initial commit

---
+++ logic.js
@@ -0,0 +1,19 @@
+
+//We will first implement a function that generates a choice for the computer, and will
+//then compare it to that of the user. The users choice will be found through element id from the html.
+//In order to compare the results, we will have to make a funciton "compare" which will establish and respond with
+//the winner. This compare function will be below the play function.
+
+
+//this is where we will find the computers choice and compare it to the users.
+var game_result;
+function play(userChoice){
+}
+
+
+
+//this is where we decalre how to compare the choices based on the basic rules of RPS.
+//think about how a player wins/loses. What is the logic? View the flowchart for reference. 
+var compare = function(choice1, choice2){
+
+}
